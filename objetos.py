8f2401dbddfc67eed918a63802ecde7eb531d71d
tumorenito19-virtual <sevasharold@gmail.com>
2024-10-09 11:35:27 +0200

    Create objetos.py

---
+++ objetos.py
@@ -0,0 +1,40 @@
+class Persona:
+      
+      politica="comunista"
+      sexo="hermafrodita"
+      ciudad="murcia"
+      religion="judio"
+      
+      def __init__(self, nombre , año):
+            self.nombre = nombre
+            self.año = año
+            self.edad= 2024-año
+      
+      def saludar(self):
+            print(f"hola me llamo{self.nombre} y tengo{self.edad}")
+
+personalvaro = Persona("alvaro", 2004)
+personalvaro.saludar()
+
+
+
+      
+
+    
+
+        
+            
+      
+
+      
+
+      
+      
+        
+    
+
+
+
+
+
+        
